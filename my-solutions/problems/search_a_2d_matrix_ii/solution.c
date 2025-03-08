bac85ea560a37852d817082e81b96ef89b8cd170
Pramoth B <717824y133@kce.ac.in>
2025-03-08 05:45:41 +0000

    Sync LeetCode submission - Search a 2D Matrix II (c)

---
+++ my-solutions/problems/search_a_2d_matrix_ii/solution.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target)
+ {
+    if (matrixSize == 0 || matrixColSize[0] == 0) 
+        return false;
+
+    int row = 0, col = matrixColSize[0] - 1; 
+
+    while (row < matrixSize && col >= 0) 
+    {
+        if (matrix[row][col] == target)
+            return true;
+        else if (matrix[row][col] > target)
+            col--; 
+        else
+            row++;  
+    }
+
+    return false;
+}
