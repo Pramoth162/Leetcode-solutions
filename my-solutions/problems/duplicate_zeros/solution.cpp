a3c8a4c01938a59ae2f316663c0e73598a80d693
Pramoth B <717824y133@kce.ac.in>
2025-04-22 12:24:16 +0000

    Sync LeetCode submission - Duplicate Zeros (cpp)

---
+++ my-solutions/problems/duplicate_zeros/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) 
+    {
+     int n = arr.size();
+        int zeros=0;
+        for(int i=0;i<n;i++)
+        {
+            if (arr[i]==0)zeros++;
+        }
+        for(int i=n-1,j=n+zeros-1;i>=0;i--,j--)
+        {
+            if(j<n)arr[j]=arr[i];
+            if (arr[i]==0)
+            {
+                j--;
+                if(j<n) arr[j]=0;   
+            }
+        }
+    }
+};
