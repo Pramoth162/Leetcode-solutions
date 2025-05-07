68f59658d6d9db8edd69075ac55bbec9e8596a4c
Pramoth B <717824y133@kce.ac.in>
2025-05-07 12:07:45 +0000

    Sync LeetCode submission - Shortest Distance to a Character (cpp)

---
+++ my-solutions/problems/shortest_distance_to_a_character/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) 
+    {
+      int n=s.size();
+      vector<int> answer(n,n);
+
+      int prev = -n;
+      for (int i=0; i<n; ++i)
+      {
+        if (s[i]==c)prev =i;
+        answer[i]=i-prev;
+        answer[i]=i-prev;
+      }  
+      prev=2*n;
+      for(int i=n-1;i>=0;--i)
+      {
+        if(s[i]==c)prev=i;
+        answer[i]=min(answer[i], abs(prev - i));
+      }
+      return answer;
+    }
+};
