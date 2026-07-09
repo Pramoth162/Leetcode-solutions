5b20d6f0ffe6ef5d689490822f798713b0c9b675
Pramoth B <717824y133@kce.ac.in>
2026-07-09 06:11:41 +0000

    Sync LeetCode submission - Jump Game (cpp)

---
+++ my-solutions/problems/jump_game/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int reach=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i<=reach)
+            {
+            reach=max(reach,i+nums[i]);
+            }
+            else
+            return false;
+        }
+        return true;
+    }
+};
