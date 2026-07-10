863fe7668b967965fbeef005db3749284926ce8e
Pramoth B <717824y133@kce.ac.in>
2026-07-10 16:37:25 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

---
+++ my-solutions/problems/reverse_integer/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev=0;
+        while(x!=0)
+        {
+            int digits=x%10;
+            if(rev>INT_MAX/10 ||(rev==INT_MAX/10 && digits>7))
+            return 0;
+            if(rev<INT_MIN/10 ||(rev==INT_MIN/10 && digits<-8))
+            return 0;
+        
+        x=x/10;
+        rev=rev*10+digits;
+        }
+        return rev;
+        
+    }
+};
