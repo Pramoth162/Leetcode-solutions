80912efa88bacd37116c3b55de31d3ec859df05c
Pramoth B <717824y133@kce.ac.in>
2026-07-13 04:53:00 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

--- my-solutions/problems/sqrt(x)/solution.cpp
+++ my-solutions/problems/sqrt(x)/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int high=x,low=0;
+        if(x==0||x==1) return x;
+        while(low<=high)
+        {
+            int m=low+(high-low)/2;
+            if(m==x/m)
+            return m;
+            else if(m<x/m)
+            low=m+1;
+            else
+            high=m-1;
+        }
+        return high;
     }
 };
