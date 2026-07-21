cd0caf1af54ce0d545d51cb9c55a5d78220c87ef
Pramoth B <717824y133@kce.ac.in>
2026-07-21 04:18:43 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- my-solutions/problems/fibonacci_number/solution.cpp
+++ my-solutions/problems/fibonacci_number/solution.cpp
@@ -2,7 +2,14 @@ class Solution {
 public:
     int fib(int n) 
     {
-        if(n<=1)return n;
-        return fib(n-1)+ fib(n-2);
+        int arr[n+1];
+        if(n<=1){
+            return n;}
+        arr[0]=0;arr[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 };
