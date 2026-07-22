e4e6cd5b4fe05eb628d056123323074e55bd96b6
Pramoth B <717824y133@kce.ac.in>
2026-07-22 08:48:35 +0000

    Sync LeetCode submission - Palindromic Substrings (cpp)

---
+++ my-solutions/problems/palindromic_substrings/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n=s.size();
+        int count=0;
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int diff=0;diff<n;diff++)
+        {
+            for(int i=0,j=i+diff;j<n;j++,i++)
+            {
+                if(i==j)
+                {
+                    dp[i][j]=1;
+                }
+                else if(diff==1)
+                {
+                    if(s[i]==s[j])
+                    {
+                        dp[i][j]=2;
+                    }
+                }
+                else
+                {
+                    if(s[i]==s[j]&&dp[i+1][j-1]!=0)
+                    dp[i][j]=2+dp[i+1][j-1];
+                }
+                if(dp[i][j]!=0)
+            count++;
+            }
+            
+        }
+        return count;
+    }
+};
