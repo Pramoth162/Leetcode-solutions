4706f9c7f971426d650e41806a7ec267d9cd5733
Pramoth B <717824y133@kce.ac.in>
2026-07-23 15:01:00 +0000

    Sync LeetCode submission - N-th Tribonacci Number (cpp)

--- my-solutions/problems/n-th_tribonacci_number/solution.cpp
+++ my-solutions/problems/n-th_tribonacci_number/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int arr[n+1];
-        if(n==0)
+        if(n<1)
         return 0;
-        if(n<=2)
+        if(n==1)
         return 1;
-        arr[0]=0;arr[1]=1;arr[2]=1;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
         for(int i=3;i<=n;i++)
         {
-        arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
-        return arr[n];
-        
+        return dp[n];
     }
 };
