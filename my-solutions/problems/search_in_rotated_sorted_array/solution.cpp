c3ce254b5426996496f2b4e099e266c166bccdc4
Pramoth B <717824y133@kce.ac.in>
2026-07-24 09:59:10 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- my-solutions/problems/search_in_rotated_sorted_array/solution.cpp
+++ my-solutions/problems/search_in_rotated_sorted_array/solution.cpp
@@ -3,10 +3,9 @@ public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
         int h=n-1,l=0;
-        int ans=-1;
         while(l<=h)
         {
-            int m=(l+h)/2;
+            int m=l+(h-l)/2;
             if(nums[m]==target)
             return m;
             else if(nums[m]>=nums[l])
@@ -24,6 +23,6 @@ public:
                  h=m-1;
             }
         }
-        return ans;
+        return -1;
     }
 };
