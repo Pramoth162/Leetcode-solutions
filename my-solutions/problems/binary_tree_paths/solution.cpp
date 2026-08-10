2fd528e9dc8b247acfc169fa60396923db83fa0e
Pramoth B <717824y133@kce.ac.in>
2026-08-10 05:32:16 +0000

    Sync LeetCode submission - Binary Tree Paths (cpp)

---
+++ my-solutions/problems/binary_tree_paths/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* node, string path,vector<string>& result)
+    {
+        if(!node)
+        return;
+        path+=to_string(node->val);
+        if(!node->left&&!node->right)
+        {
+        result.push_back(path);
+        return;
+        }
+        if(node->left)
+        dfs(node->left,path+"->",result);
+        if(node->right)
+        dfs(node->right,path+"->",result);
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        if(root)
+        dfs(root,"",result);
+        return result;
+    }
+};
